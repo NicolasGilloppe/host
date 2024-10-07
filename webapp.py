7d12f2bdc42677bd5f5f08e06ed87226bd1866b9
NicolasGilloppe <143018427+NicolasGilloppe@users.noreply.github.com>
2024-10-07 20:54:22 +0900

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import requests
+import time
 
 def main():
     uploaded_file = st.file_uploader("Import an XLSX file", type="xlsx")
@@ -41,7 +42,8 @@ def main():
             with st.spinner("Scraping data from Google Maps..."):
                 # Send a request to your backend
                 headers = {'Connection': 'keep-alive'}
-                response = requests.post('http://194.164.72.188:4000/scrape', json={'url': req}, headers=header, timeout=1200s)
+                time.sleep(0.01)
+                response = requests.post('http://194.164.72.188:4000/scrape', json={'url': req}, headers=header, timeout=1200)
                 
                 # Check if the response is successful
                 if response.status_code == 200:
